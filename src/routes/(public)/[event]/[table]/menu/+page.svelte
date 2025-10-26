96187236697c59c9c362da4d0868ae9b4ad1ac37
kevinloffler <hi@kevinloeffler.ch>
2025-10-26 14:49:26 +0100

    add empty state add navbar

--- src/routes/(public)/[event]/[table]/menu/+page.svelte
+++ src/routes/(public)/[event]/[table]/menu/+page.svelte
@@ -9,13 +9,21 @@
         <p>{article.description}</p>
         <button onclick={() => addItem(article)}>Hinzufügen</button>
     </div>
+{:else}
+    <p>Keine Artikel vorhanden.</p>
 {/each}
+
+
+<Navbar>
+    <a href={`/${data.event?.name}/${data.table?.name}`} class="button-main button-secondary">Zurück</a>
+</Navbar>
 
 
 <script lang="ts">
 
     import type {PageProps} from './$types'
     import type {ArticleDTO} from '$lib/db/models'
+    import Navbar from '$lib/components/navbar.svelte';
 
     let { data }: PageProps = $props()
     const articles: ArticleDTO[] = data.articles!
@@ -34,6 +42,10 @@
 
 
 <style>
+
+    h1 {
+        margin-bottom: 20px;
+    }
 
     .article {
         border: 1px solid var(--secondary-color);
